105df018b43e87bba39b234b547594db1c1a6239
Mohamed Rezk <mohamed.rezk@orbyta.it>
2022-03-02 14:40:55 +0100

    added detail product page's logic

--- src/app/main/pages/detail-product-page/detail-product-page.component.ts
+++ src/app/main/pages/detail-product-page/detail-product-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { Route, Router } from "@angular/router";
+import { Product } from "../../models/product.model";
+import { ProductsService } from "../../services/products.service";
 
 @Component({
     selector: 'detail-product-page-component',
@@ -6,5 +9,29 @@ import { Component } from "@angular/core";
     styleUrls: ['./detail-product-page.component.css']
 })
 export class DetailProductPageComponent {
+    startSearch: number = 10;
+    idProduct: string = this.router.url.substring(this.startSearch, this.router.url.length);
+    product!: Product;
+    productLoaded: boolean = false;
+    constructor(private router: Router,
+                private productsService: ProductsService){}
 
+    ngOnInit(){
+        console.log(this.idProduct);
+        this.productsService.fetchProductById(this.idProduct).subscribe({
+            next: (response) => {
+                console.log(response);
+                let prod: Product = new Product(response.idProduct, response.category, 
+                    response.idProfile, response.img, response.name, 
+                    response.price, response.quantity);
+                this.product = prod;
+            },
+            error: (err) => {
+                console.log(err);
+            },
+            complete: () => {
+                this.productLoaded = true;
+            }
+        })
+    }
 }
