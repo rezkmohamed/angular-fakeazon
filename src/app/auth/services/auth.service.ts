5d09b5627119b88f14ee0e2deb7a433bd5f53ded
Mohamed Rezk <mohamed.rezk@orbyta.it>
2022-03-03 16:25:59 +0100

    added auth guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 import { RegisterProfile } from "src/app/main/models/register-profile.model";
 import { LoginProfile } from "../models/login.model";
+import { User } from "../models/user.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
     private urlBase: string = "http://localhost:8080/";
+    user = new BehaviorSubject<User>(new User("", "", ""));
 
     constructor(private http: HttpClient) {}
 
